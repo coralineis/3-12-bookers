dc410d60e5298d1a3d67c1c9b7ca2777904303c1
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-04 12:48:25 +0000

    [Update] validation追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,10 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     if book.save
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(book.id)
+     else
+       render :new
     end
   end
 
@@ -35,7 +37,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     if book.destroy
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully destroyed."
       redirect_to '/books'
     end
   end
