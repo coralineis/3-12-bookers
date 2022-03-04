cf7871dc633e8bedd2700989bd0324eed0068d86
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-04 04:41:37 +0000

    [Add] top,indexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -10,4 +18,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
