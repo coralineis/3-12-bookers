a26423751379ed10c801cc36b149cc0cffdd9e33
EC2 Default User <ec2-user@ip-172-31-38-45.ec2.internal>
2022-03-04 02:01:51 +0000

    [Add] アプリトップ画面のレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'homes/top'
   resources :books
-  root to: 'books#index'
+  root to: 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
